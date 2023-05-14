2425fbb7435a3d40855168a2e7b53831ef2a5589
Wil Ifan Evans <w.evans@posteo.net>
2023-05-14 18:50:25 +0100

    Turn on raw mode.

--- sgwennu.c
+++ sgwennu.c
@@ -18,11 +18,29 @@ void enableRawMode() {	// Turns off echoing in the terminal.
 	struct termios raw = orig_termios;
 
 	/*
-	~ is the bitwise NOT operator, which here results in echo mode
-	being disabled. It also disables canonical mode, which means
-	input is read byte-by-byte rather than line-by-line.
-	*/ 
-	raw.c_lflag &= ~(ECHO | ICANON);
+	~ is the bitwise NOT operator. This reverses the bits of 
+	flags within parentheses, which effectively disables each
+	one.
+	There are two groups of flags:
+		* Input flags (c_iflag)
+		* Output flags (c_oflag)
+		* Local flags (c_lflag)
+	ECHO: disable printing to the screen
+	ICANON: disable canonical mode, which means input is now read
+	byte-by-byte rather than line-by-line
+	ISIG: disable SIGINT signals like Ctrl-C and Ctrl-Z
+	IXON: disable flow control with Ctrl-S & Ctrl-Q (XON/XOFF)
+	IEXTEN: disable Ctrl-V being reserved for literal input
+	ICRNL: disable Ctrl-M as newline (NL) not carriage return (CR)
+	OPOST: disable all output processing, which stops \n -> \r\n
+	Misc flags are BRKINT, INPCK, ISTRIP and CS8. None of these
+	probably required in modern terminal emulators but are part
+	of enabling "raw mode" for historical reasons.
+	*/
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8); // | is bitwise-OR operator
+	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
@@ -34,9 +52,9 @@ int main() {
 	// Displays keypresses unless q pressed, in which case quit.
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
 		if (iscntrl(c)) {
-			printf("%d\n", c);
+			printf("%d\r\n", c); // Need carriage return + newline
 		} else {
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
 	}
 
